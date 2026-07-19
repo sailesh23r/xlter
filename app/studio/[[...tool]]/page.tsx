6a3e69012f681be09c0e1429935a35a4bad2c300
xeltrcom <admin@xeltr.com>
2026-07-19 21:02:45 +0530

    login1

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -11,7 +11,7 @@ import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 import '../studio.css'
 
-export const dynamic = 'force-static'
+export const dynamic = 'force-dynamic'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
